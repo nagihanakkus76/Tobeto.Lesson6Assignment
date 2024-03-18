33a8bc87d6b346046d5b4c51102bed1f58f91291
nagihanakkus <nagihan.akkus.76@gmail.com>
2024-03-18 22:07:47 +0300

    Polymorfizm1 created

---
+++ Tobeto.Lesson6Assignment/Entity/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tobeto.Lesson6Assignment.Entity
+{
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Hav hav!");
+        }
+    }
+}
--- Tobeto.Lesson6Assignment/Program.cs
+++ Tobeto.Lesson6Assignment/Program.cs
@@ -1,1 +1,15 @@
-﻿// Ödev ayrı branch ler altında yapılmıştır.
+﻿using Tobeto.Lesson6Assignment.Entity;
+
+Console.WriteLine("\n 1- Kedi \n 2- Köpek\nLütfen bir hayvan seçiniz:");
+string data =Console.ReadLine();
+if (data == "1" || data == "Kedi" || data == "kedi")
+{
+    Animal cat = new Cat();
+    cat.MakeSound();
+}
+else if (data == "2" || data == "Köpek" || data == "köpek")
+{
+    Animal dog = new Dog();
+    dog.MakeSound();
+}
+else { Console.WriteLine("Hatalı seçim"); }
